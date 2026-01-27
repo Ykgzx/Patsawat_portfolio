83c92c5a5f5a340f8baf23fbbb92f76347b8719b
Ykgzx <duu75512@gmail.com>
2026-01-27 21:02:40 +0700

    feat: Implement core portfolio pages (Home, About, Skills, Tools, Project, Contact) and sidebar navigation with routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/project/project.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ToolsComponent } from './pages/tools/tools.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'project', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'tools', component: ToolsComponent },
 ];
