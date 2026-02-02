20fee9017c9845039265af6fd14c244f72940c22
Ykgzx <duu75512@gmail.com>
2026-02-02 19:36:54 +0700

    feat: Implement initial responsive layout with a responsive sidebar navigation component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-test');
+  sidebarOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
+  }
 }
